9ea7ac1de364736687eae7d66354bd6ed5998769
Alex Rhee <alex.rhee@gmail.com>
2017-01-16 15:56:19 -0800

    removed hard coded files

--- gen_new_music.py
+++ gen_new_music.py
@@ -10,10 +10,7 @@ from mido import MidiFile
 # mat = new_song_mat(model, psg_seed)
 # track = notelist_to_track(mat)
 
-def load_model():
-    model_dir = 'data/models/'
-    model_name = 'test'
-
+def load_model(model_dir, model_name):
     # load json and create model
     with open(model_dir + model_name + '.json', 'r') as json_file:
         loaded_model_json = json_file.read()
@@ -40,48 +37,33 @@ def load_sample_psg():
     psg_seed = psg_seed[None, :]
     return psg_seed
 
-model = load_model()
-psg = load_sample_psg()
+def main():
+    model_dir = opts.model_dir
+    model_name = opts.model_name
+    model = load_model(model_dir, model_name)
+    psg = load_sample_psg()
 
-mmat = new_song_mat(model, psg)
-track = notelist_to_track(mmat, tick_step=32)
-mid = MidiFile()
-mid.tracks.append(track)
-mid.save('new_song.mid')
+    mmat = new_song_mat(model, psg)
+    track = notelist_to_track(mmat, tick_step=32)
+    mid = MidiFile()
+    mid.tracks.append(track)
+    mid.save(opts.outfile)
 
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Trains model on data')
+    parser.add_argument('-d', '--model_dir', action='store', required=True,
+        help='filename for model params')
+    parser.add_argument('-m', '--model', action='store', required=True,
+        help='filename for model params')
+    parser.add_argument('-o', '--outfile', action='store', required=True,
+        help='filename for new song')
 
-# def main():
-#     with open(opts.data) as f:
-#         data = pickle.load(f)
-#
-#     X = data['X']
-#     y = data['y']
-#
-#     train_len = X.shape[1]
-#     model = default_model(train_len)
-#     model.fit(X,y, batch_size=50, nb_epoch=1)
-#
-#     print 'saving data'
-#     with open(opts.outfile + '.json', 'w') as json_file:
-#         json_file.write(model.to_json())
-#     model.save_weights(opts.outfile + '.h5')
-#
-#
-# if __name__ == '__main__':
-#     parser = argparse.ArgumentParser(description='Trains model on data')
-#     parser.add_argument('-d', '--data', action='store', required=True,
-#         help='pickle data to train on')
-#     parser.add_argument('-m', '--model', action='store', required=True,
-#         help='filename for model params')
-#     parser.add_argument('-o', '--outfile', action='store', required=True,
-#         help='filename for new song')
-#
-#     opts, args = parser.parse_known_args(sys.argv[1:])
-#     main()
-#
-#
-#     # save the file
-#     mid = MidiFile()
-#     mid.tracks.append(track)
-#     mid.save('new_song.mid')
+    opts, args = parser.parse_known_args(sys.argv[1:])
+    main()
+
+
+    # save the file
+    mid = MidiFile()
+    mid.tracks.append(track)
+    mid.save('new_song.mid')
